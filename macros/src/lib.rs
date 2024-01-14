bbb88da09eb68b8f0bdf4953d4b41c22b58b2040
Izzy Lancaster <izzylanfox@gmail.com>
2024-01-13 23:31:31 -0800

    Redo the way MacroArgs are parsed

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -11,45 +11,87 @@ use quote::quote;
 use syn::ExprLit;
 use syn::LitBool;
 use syn::LitStr;
-use syn::parse::Parse;
 use syn::parse_macro_input;
-use syn::Attribute;
 use syn::Expr;
 use syn::ItemFn;
 use syn::Lit;
 use syn::Meta;
+use syn::punctuated::Punctuated;
+use syn::token::Comma;
+
+struct MacroArgs {
+  default_log_filter: Option<String>,
+  ansi: bool
+}
+
+impl Default for MacroArgs {
+  fn default() -> Self {
+    Self { 
+      default_log_filter: Default::default(),
+      ansi: true
+    }
+  }
+}
+
+impl MacroArgs {
+  fn from_punctuated(punctuated: Punctuated<Meta, Comma>) -> syn::Result<Self> {
+    let mut new_self = Self::default();
+    
+    for m in punctuated {
+      let name_value =  m.require_name_value().map_err(map_name_value_error)?;
+      let ident = name_value.path.require_ident().map_err(map_name_value_error)?;
+      match ident.to_string().as_str() {
+        "default_log_filter" => new_self.default_log_filter = Some(require_lit_str(&name_value.value)?.value()),
+        "ansi" => new_self.ansi = require_lit_bool(&name_value.value)?.value(),
+        _ => return Err(syn::Error::new_spanned(
+          &name_value.path,
+          "Unrecognized attribute, see documentation for details.",
+        ))
+      };
+    }
+
+    Ok(new_self)
+  }
+}
+
+fn map_name_value_error(err: syn::Error) -> syn::Error {
+  syn::Error::new(err.span(), "Expected NameValue syntax, e.g. 'default_log_filter = \"debug\"'.")
+}
+
+fn require_lit_str(expr: &Expr) -> syn::Result<&LitStr> {
+  match expr {
+    Expr::Lit(ExprLit { lit: Lit::Str(lit_str), .. }) => Ok(lit_str),
+    _ => Err(syn::Error::new_spanned(
+      &expr,
+      "Failed to parse value, expected a string",
+    ))
+  }
+}
+
+fn require_lit_bool(expr: &Expr) -> syn::Result<&LitBool> {
+  match expr {
+      Expr::Lit(ExprLit { lit: Lit::Bool(lit_bool), .. }) => Ok(lit_bool),
+      _ => Err(syn::Error::new_spanned(
+        &expr,
+        "Failed to parse value, expected a bool",
+      ))
+  }
+}
 
 
 #[proc_macro_attribute]
-pub fn test(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn test(args: TokenStream, item: TokenStream) -> TokenStream {
+  let punctuated_args = parse_macro_input!(args with Punctuated::<Meta, syn::Token![,]>::parse_terminated);
   let item = parse_macro_input!(item as ItemFn);
-  try_test(attr, item)
+
+  try_test(punctuated_args, item)
     .unwrap_or_else(syn::Error::into_compile_error)
     .into()
 }
 
-fn parse_attrs(attrs: Vec<Attribute>) -> syn::Result<(AttributeArgs, Vec<Attribute>)> {
-  let mut attribute_args = AttributeArgs::default();
-  let mut ignored_attrs = vec![];
-  for attr in attrs {
-    let matched = attribute_args.try_parse_attr_single(&attr)?;
-    // Keep only attrs that didn't match the #[test_log(_)] syntax.
-    if !matched {
-      ignored_attrs.push(attr);
-    }
-  }
-
-  Ok((attribute_args, ignored_attrs))
+fn try_test(punctuated_args: Punctuated<Meta, Comma>, input: ItemFn) -> syn::Result<Tokens> {
+  let macro_args = MacroArgs::from_punctuated(punctuated_args)?;
   
-}
-
-fn try_test(attr: TokenStream, input: ItemFn) -> syn::Result<Tokens> {
-  let inner_test = if attr.is_empty() {
-    quote! { ::core::prelude::v1::test }
-  } else {
-    attr.into()
-  };
-
   let ItemFn {
     attrs,
     vis,
@@ -57,13 +99,11 @@ fn try_test(attr: TokenStream, input: ItemFn) -> syn::Result<Tokens> {
     block,
   } = input;
 
-  let (attribute_args, ignored_attrs) = parse_attrs(attrs)?;
-  let logging_init = expand_logging_init(&attribute_args);
-  let tracing_init = expand_tracing_init(&attribute_args);
+  let logging_init = expand_logging_init(&macro_args);
+  let tracing_init = expand_tracing_init(&macro_args);
 
   let result = quote! {
-    #[#inner_test]
-    #(#ignored_attrs)*
+    #(#attrs)*
     #vis #sig {
       // We put all initialization code into a separate module here in
       // order to prevent potential ambiguities that could result in
@@ -91,69 +131,9 @@ fn try_test(attr: TokenStream, input: ItemFn) -> syn::Result<Tokens> {
 }
 
 
-#[derive(Debug)]
-struct AttributeArgs {
-  default_log_filter: Option<String>,
-  ansi: bool
-}
-
-impl Default for AttributeArgs {
-    fn default() -> Self {
-        Self { 
-          default_log_filter: Default::default(),
-          ansi: true
-        }
-    }
-}
-
-impl AttributeArgs {
-  fn try_parse_attr_single(&mut self, attr: &Attribute) -> syn::Result<bool> {
-    let nested_meta = attr.parse_args_with(Meta::parse)?;
-    let name_value = nested_meta.require_name_value().map_err(map_name_value_error)?;
-    let ident = name_value.path.require_ident().map_err(map_name_value_error)?;
-
-    match ident.to_string().as_str() {
-      "default_log_filter" => self.default_log_filter = Some(require_lit_str(&name_value.value)?.value()),
-      "ansi" => self.ansi = require_lit_bool(&name_value.value)?.value(),
-      _ => return Err(syn::Error::new_spanned(
-        &name_value.path,
-        "Unrecognized attribute, see documentation for details.",
-      ))
-    };
-
-    Ok(true)
-  }
-}
-
-
-fn map_name_value_error(err: syn::Error) -> syn::Error {
-  syn::Error::new(err.span(), "Expected NameValue syntax, e.g. 'default_log_filter = \"debug\"'.")
-}
-
-fn require_lit_str(expr: &Expr) -> syn::Result<&LitStr> {
-  match expr {
-    Expr::Lit(ExprLit { lit: Lit::Str(lit_str), .. }) => Ok(lit_str),
-    _ => Err(syn::Error::new_spanned(
-      &expr,
-      "Failed to parse value, expected a string",
-    ))
-  }
-}
-
-fn require_lit_bool(expr: &Expr) -> syn::Result<&LitBool> {
-  match expr {
-      Expr::Lit(ExprLit { lit: Lit::Bool(lit_bool), .. }) => Ok(lit_bool),
-      _ => Err(syn::Error::new_spanned(
-        &expr,
-        "Failed to parse value, expected a bool",
-      ))
-  }
-}
-
-
 /// Expand the initialization code for the `log` crate.
 #[cfg(feature = "log")]
-fn expand_logging_init(attribute_args: &AttributeArgs) -> Tokens {
+fn expand_logging_init(attribute_args: &MacroArgs) -> Tokens {
   let add_default_log_filter = if let Some(default_log_filter) = &attribute_args.default_log_filter
   {
     quote! {
@@ -174,13 +154,13 @@ fn expand_logging_init(attribute_args: &AttributeArgs) -> Tokens {
 }
 
 #[cfg(not(feature = "log"))]
-fn expand_logging_init(_attribute_args: &AttributeArgs) -> Tokens {
+fn expand_logging_init(_attribute_args: &MacroArgs) -> Tokens {
   quote! {}
 }
 
 /// Expand the initialization code for the `tracing` crate.
 #[cfg(feature = "trace")]
-fn expand_tracing_init(attribute_args: &AttributeArgs) -> Tokens {
+fn expand_tracing_init(attribute_args: &MacroArgs) -> Tokens {
   let env_filter = if let Some(default_log_filter) = &attribute_args.default_log_filter {
     quote! {
       ::test_log::tracing_subscriber::EnvFilter::builder()
@@ -237,6 +217,6 @@ fn expand_tracing_init(attribute_args: &AttributeArgs) -> Tokens {
 }
 
 #[cfg(not(feature = "trace"))]
-fn expand_tracing_init(_attribute_args: &AttributeArgs) -> Tokens {
+fn expand_tracing_init(_attribute_args: &MacroArgs) -> Tokens {
   quote! {}
 }
