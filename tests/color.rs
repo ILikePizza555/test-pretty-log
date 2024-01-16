3953e6193c202822e5da8091e32a517f9e6ac538
Izzy Lancaster <izzylanfox@gmail.com>
2024-01-15 17:27:48 -0800

    Added tests for color formatting

---
+++ tests/color.rs
@@ -0,0 +1,20 @@
+use tracing::{info, debug};
+
+/// Manually verify this with the following command
+/// ```sh
+/// RUST_LOG=debug cargo test --test color --features trace -- --nocapture
+/// ```
+
+#[test_pretty_log::test(color = false)]
+fn trace_with_color_off() {
+  info!("This should NOT be colored");
+  assert!(true);
+  debug!("done");
+}
+
+#[test_pretty_log::test]
+fn trace_with_color() {
+  info!("This SHOULD be colored");
+  assert!(true);
+  debug!("done");
+}
