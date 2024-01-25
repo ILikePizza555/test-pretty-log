8d56ad78e5ce97b92dedd3bbef058091bd2e4aff
Izzy Lancaster <izzylanfox@gmail.com>
2024-01-24 19:08:56 -0800

    Just gonna remove env_logger

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,10 +1,10 @@
 use std::env::var_os;
 
 pub fn env_var(key: &str) -> Option<String> {
+    let err_msg = format!("test-pretty-log: {} must be valid UTF-8", key);
     var_os(key).map(
         |oss| oss
             .to_ascii_lowercase()
-            .to_str()
-            .expect(format!("test-pretty-log: {} must be valid UTF-8", key).as_str())
-            .to_owned())
+            .into_string()
+            .expect(&err_msg))
 }
